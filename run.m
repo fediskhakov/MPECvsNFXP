352675db13718eb533f6260796ce5267f7193058
Fedor Iskhakov <f@iskh.ru>
2014-04-03 21:02:41 +1100

    Starting the script to run everything for the Iskhakov,Rust,Schjerning comment

---
+++ run.m
@@ -0,0 +1,9 @@
+% This is the main script to create all the data for the
+% Iskhakov,Rust,Schjerning comment for Judd and Su 2012 Econometrica
+
+%add path to the functions
+currdir=pwd;
+addpath([currdir filesep 'matlab_include']);
+
+
+
