aec9e9292b4ccce84ab0c799c3d34c71575a7187
Fedor Iskhakov <f@iskh.ru>
2014-11-08 19:43:33 +0100

    Solving recentering issue in the MPEC/Matlab code

--- juddsu/confuneq.m
+++ juddsu/confuneq.m
@@ -7,6 +7,7 @@ function [c,ceq,DC,DCeq] = confuneq(X)
 % source: Su and Judd (2011), Constrained Optimization Approaches to
 % Estimation of Structural Models.
 % Code Revised: Che-Lin Su, May 2010.
+% Code Revised: Iskhakov, Rust, Schjerning, October 2014
 
 global N M beta CbEVPrime TransProbPrime RPrime EVPrime;
 
@@ -17,11 +18,13 @@ global N M beta CbEVPrime TransProbPrime RPrime EVPrime;
 indices = repmat((1:N)',1,M)+repmat((1:M),N,1)-1;
 
 CbEV(N+1:N+M)=CbEV(N);
-
-s1 = exp(CbEV(indices));
-s2 = exp(-RC+CbEV(1));
+v1=CbEV(indices);
+v2=-RC+CbEV(1);
+vmax=max(v1,v2);
+s1 = exp(v1-vmax);
+s2 = exp(v2-vmax);
 s =  s1 + s2;
-logs = log(s);
+logs = vmax+log(s);
 BellmanViola = logs * TransProb - EV;
 
 % Defne and evaluate nonlinear inequality constraints
@@ -33,7 +36,9 @@ ceq = BellmanViola;
     if nargout > 3
         DC= [];
     
-        d1 = ((CbEVPrime.*repmat(exp(CbEV),1,length(X)) + exp(-RC+CbEV(1))*repmat(RPrime,N+M,1)))./(repmat(exp(CbEV)+exp(-RC+CbEV(1)),1,length(X)));
+        %d1 = ((CbEVPrime.*repmat(exp(CbEV),1,length(X)) + exp(-RC+CbEV(1))*repmat(RPrime,N+M,1)))./(repmat(exp(CbEV)+exp(-RC+CbEV(1)),1,length(X)));
+        maxCbEV=max([CbEV;-RC+CbEV(1)]);%scalar
+        d1 = ((CbEVPrime.*repmat(exp(CbEV-maxCbEV),1,length(X)) + exp(-RC+CbEV(1)-maxCbEV)*repmat(RPrime,N+M,1)))./(repmat(exp(CbEV-maxCbEV)+exp(-RC+CbEV(1)-maxCbEV),1,length(X)));
         sum1 =  reshape(sum(reshape(d1(indices',:) .* repmat(TransProb,N,length(X)),M,N, length(X) )),N,length(X)); 
         sum2 = logs*TransProbPrime';
         DCeq = sum1 + sum2 - EVPrime;
